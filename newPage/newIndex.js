e0d2773ced9e9e018c1140da90ca6d234beef77a
Knight4Lancelot <320062713@qq.com>
2022-07-27 13:37:25 +0800

    2022/7/27修改

--- newPage/newIndex.js
+++ newPage/newIndex.js
@@ -2,6 +2,14 @@ var pageWidth, pageHeight;
 var inputFocus = false;
 var btnHover = false;
 var inputHover = false;
+var btnListLocation = {
+	left: [],
+	top: []
+};
+var hiddenNameList = {
+	btn: [],
+	span: []
+};
 
 var body = document.getElementById('main-body');
 var backgroundImg = document.getElementById('body-background');
@@ -12,15 +20,37 @@ var dateClock1 = document.getElementById('date-clock-1');
 var dateClock2 = document.getElementById('date-clock-2');
 var timeClock = document.getElementById('time-clock');
 var tools = document.getElementById('tools');
+// 功能按钮与功能名称描述集合
+var btnList = document.getElementsByClassName('function-entries');
+var nameList = document.getElementsByClassName('hover-show-function-name');
+// 常用功能列表元素
+var webCollections = document.getElementById('web-collections');
+var webCollectionsName = document.getElementById('web-collections-name');
+var setBackground = document.getElementById('set-background');
+var setBackgroundName = document.getElementById('set-background-name');
+var moreFunctions = document.getElementById('more-functions');
+var moreFunctionsName = document.getElementById('more-functions-name');
 
 body.onload = init;
-searchBtn.onmouseover = searchBtnOver;
-searchBtn.onmouseout = searchBtnOut;
+searchBtn.onmouseover = function() { searchBtnStatus(true); }
+searchBtn.onmouseout = function() { searchBtnStatus(false); }
 searchBtn.onclick = jump2URL;
-searchInput.onfocus = searchInputFocus;
-searchInput.onblur = searchInputBlur;
-searchInput.onmouseover = searchInputOver;
-searchInput.onmouseout = searchInputOut;
+searchInput.onfocus = function() { searchInputIsFocus(true); }
+searchInput.onblur = function() { searchInputIsFocus(false); }
+searchInput.onmouseover = function() { searchInputIsOver(true); }
+searchInput.onmouseout = function() { searchInputIsOver(false); }
+// 常用功能列表的函数设置
+webCollections.onmouseover = function() { changeBtnSize(0, true); }
+webCollections.onmouseout = function() { changeBtnSize(0, false); }
+setBackground.onmouseover = function() { changeBtnSize(1, true); }
+setBackground.onmouseout = function() { changeBtnSize(1, false); }
+moreFunctions.onmouseover = function() { changeBtnSize(2, true); }
+moreFunctions.onmouseout = function() { changeBtnSize(2, false); }
+
+for (let i = 0; i < nameList.length; i++) {
+	nameList[i].onmouseover = function() { changeNameVisible(i, true); }
+	nameList[i].onmouseout = function() { changeNameVisible(i, false); }	
+}
 
 function init() {
 	pageHeight=document.documentElement.clientHeight;
@@ -50,9 +80,20 @@ function init() {
 	searchInput.style.left = String((pageWidth - 564)/2) + 'px';
 	searchBtn.style.left = String((pageWidth - 564)/2 + 484) + 'px';
 	timeClock.style.left = String((pageWidth - 370)/2-10) + 'px';
-	dateClock1.style.left = String((pageWidth-140)/2) + 'px';
-	dateClock2.style.left = String((pageWidth - 73)/2) + 'px';
-	tools.style.left = String((pageWidth - 750)/2) + 'px';
+	dateClock1.style.left = String((pageWidth-155)/2) + 'px';
+	dateClock2.style.left = String((pageWidth - 120)/2) + 'px';
+	tools.style.left = String((pageWidth - 790)/2) + 'px';
+	// 功能按钮定位
+	for (var i = 0; i < btnList.length; i++) {
+		btnListLocation.left.push(20+(i%5) * 150);
+		btnListLocation.top.push(20+(i<5?0:1) * 150);
+		btnList[i].style.left = String(btnListLocation.left[i]) + 'px';
+		btnList[i].style.top = String(btnListLocation.top[i]) + 'px';
+		nameList[i].style.left = String(btnListLocation.left[i]+12) + 'px';
+		nameList[i].style.top = String(btnListLocation.top[i]+50) + 'px';
+		hiddenNameList.btn.push(false);
+		hiddenNameList.span.push(false);
+	}
 }
 
 function setInputShow(status) {
@@ -98,37 +139,20 @@ function jump2URL() {
 	}
 }
 
-function searchBtnOver() {
-	btnHover = true;
+function searchBtnStatus(status) {
+	btnHover = status;
 	setBtnIconShow(btnHover);
 	setSearchPartOpacity(btnHover||inputFocus||inputHover);
 }
 
-function searchBtnOut() {
-	btnHover = false;
-	setBtnIconShow(btnHover);
-	setSearchPartOpacity(btnHover||inputFocus||inputHover);
-}
-
-function searchInputFocus() {
-	inputFocus = true;
+function searchInputIsFocus(status) {
+	inputFocus = status;
 	setInputShow(inputFocus);
 	setSearchPartOpacity(btnHover||inputFocus||inputHover);
 }
 
-function searchInputBlur() {
-	inputFocus = false;
-	setInputShow(inputFocus);
-	setSearchPartOpacity(btnHover||inputFocus||inputHover);
-}
-
-function searchInputOver() {
-	inputHover = true;
-	setSearchPartOpacity(btnHover||inputFocus||inputHover);
-}
-
-function searchInputOut() {
-	inputHover = false;
+function searchInputIsOver(status) {
+	inputHover = status;
 	setSearchPartOpacity(btnHover||inputFocus||inputHover);
 }
 
@@ -145,4 +169,35 @@ function freshTime() {
 	if (second<10) second = "0" + second;
 	var time = hour+" : "+minute+" : "+second;
 	timeClock.innerText = time;
+}
+
+function changeBtnSize(index, status) {
+	hiddenNameList.btn[index] = status;
+	ShowNameInfo(index, hiddenNameList.span[index]||hiddenNameList.btn[index]);
+}
+
+function changeNameVisible(index, status) {
+	hiddenNameList.span[index] = status;
+	ShowNameInfo(index, hiddenNameList.span[index]||hiddenNameList.btn[index]);
+}
+
+function ShowNameInfo(index, status) {
+	nameList[index].style.visibility = status ? 'visible' : 'hidden';
+	if (status) {
+		btnList[index].style.width = '120px';
+		btnList[index].style.height = '120px';
+		btnList[index].style.opacity = 1;
+		btnList[index].style.left = String(btnListLocation.left[index]-20)+'px';
+		btnList[index].style.top = String(btnListLocation.top[index]-20)+'px';
+		btnList[index].children[0].style.left = '42px';
+		btnList[index].children[0].style.top = '30px';
+	} else {
+		btnList[index].style.width = '80px';
+		btnList[index].style.height = '80px';
+		btnList[index].style.opacity = 0.7;
+		btnList[index].style.left = String(btnListLocation.left[index])+'px';
+		btnList[index].style.top = String(btnListLocation.top[index])+'px';
+		btnList[index].children[0].style.left = '22px';
+		btnList[index].children[0].style.top = '22px';
+	}
 }
